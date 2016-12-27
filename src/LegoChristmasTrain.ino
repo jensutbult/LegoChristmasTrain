c4dbc54f44ebf1eb1346c05844e5f23d950295da
Jens Utbult <jens@izettle.com>
2016-12-27 17:03:14 +0100

    Renamed vars to match their lights in the xmas landscape

--- src/LegoChristmasTrain.ino
+++ src/LegoChristmasTrain.ino
@@ -5,12 +5,18 @@
 
 PowerFunctions pf(5, 0);
 LegoTrain train(pf, 4);
-CandleLED ledOne(6);
+CandleLED lampPostAndReindeer(6, CANDLE, 0.1);
+CandleLED fire(9, FIRE, 1.0);
+CandleLED twinLights(11, CANDLE, 0.1);
+CandleLED upperFloorAndLight(10, CANDLE, 0.3);
 
 void setup() {
 }
 
 void loop() {
   train.update();
-  ledOne.update();
+  lampPostAndReindeer.update();
+  fire.update();
+  twinLights.update();
+  upperFloorAndLight.update();
 }
